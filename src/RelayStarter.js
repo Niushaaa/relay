2dee5ed5a6948214e3d7d0e4cbe4670dea43b123
mahyard <mahyar.daneshpajouh@gmail.com>
2021-03-24 15:11:50 -0700

    pow check added

--- src/RelayStarter.js
+++ src/RelayStarter.js
@@ -2,10 +2,10 @@ var Web3 = require('web3');
 const EthereumBlock = require('ethereumjs-block/from-rpc');
 const rlp = require('rlp');
 
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/v3/6eca357aaabe473bbbaee64211ab3a14'));
 
 async function start() {
-    _block = await web3.eth.getBlock()
+    _block = await web3.eth.getBlock('latest')
     var block = new EthereumBlock(_block)
     console.log('0x' + rlp.encode(block.header.raw).toString('hex'))
     console.log(_block)
